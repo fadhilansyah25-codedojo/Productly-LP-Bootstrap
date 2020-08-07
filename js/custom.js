d27992784a439040e2ec50a14527cd6e0859711c
fadhilansyah25 <fadhilansyah25@gmail.com>
2020-08-07 21:13:42 +0700

    efek animasi section 4 dan 5 selesai

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,14 @@ $('.service .btn-primary').hide();
 // hide section 3
 $('.section-3 #image-section-3').hide();
 $('.section-3 #desc-section-3').hide();
+
+// hide section 4
+$('.section-4 #image-section-4').hide();
+$('.section-4 #desc-section-4').hide();
+
+// hide section 5
+$('.section-5 #image-section-5').hide();
+$('.section-5 #desc-section-5').hide();
 // -- end Hide Component and Element --
 
 // scroll animate
@@ -26,5 +34,17 @@ $(window).scroll(function () {
     if ( wScroll > $('.section-3 ').offset().top - 700) {
         $('.section-3 #image-section-3').show().addClass('animate__animated animate__fadeInRight');
         $('.section-3 #desc-section-3').show().addClass('animate__animated animate__fadeInLeft');
+    };
+
+    // scroll animatee section 4
+    if ( wScroll > $('.section-4 ').offset().top - 700) {
+        $('.section-4 #image-section-4').show().addClass('animate__animated animate__fadeInLeft');
+        $('.section-4 #desc-section-4').show().addClass('animate__animated animate__fadeInRight');
+    };
+
+    // scroll animatee section 5
+    if ( wScroll > $('.section-5 ').offset().top - 700) {
+        $('.section-5 #image-section-5').show().addClass('animate__animated animate__fadeInRight');
+        $('.section-5 #desc-section-5').show().addClass('animate__animated animate__fadeInLeft');
     };
 });
