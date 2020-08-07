595734781b24248583aef2ad6ac61c1b06b193a5
fadhilansyah25 <fadhilansyah25@gmail.com>
2020-08-07 20:54:53 +0700

    efek animasi section 3 selesai

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,30 @@
+// -- Hide Component and Element -- 
+// hide section service
 $('.service .card').each(function (i) {
     $('.service .card').eq(i).hide();
 });
 $('.service .btn-primary').hide();
 
+// hide section 3
+$('.section-3 #image-section-3').hide();
+$('.section-3 #desc-section-3').hide();
+// -- end Hide Component and Element --
+
+// scroll animate
 $(window).scroll(function () {
     const wScroll = $(this).scrollTop();
 
+    // scroll animate section service
     if ( wScroll > $('.service ').offset().top - 300) {
         $('.service .card').each(function (i) {
             $('.service .card').eq(i).show().addClass('animate__animated animate__fadeInUp');
         });
         $('.service .btn-primary').show().addClass('animate__animated animate__fadeInUp');
+    };
+
+    // scroll animatee section 3
+    if ( wScroll > $('.section-3 ').offset().top - 700) {
+        $('.section-3 #image-section-3').show().addClass('animate__animated animate__fadeInRight');
+        $('.section-3 #desc-section-3').show().addClass('animate__animated animate__fadeInLeft');
     };
 });
